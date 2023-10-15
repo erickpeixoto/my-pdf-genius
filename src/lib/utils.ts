26c40c8c4739df802769d0f54022ca66935fd3e3
Erick Eduardo <erickepeixoto@gmail.com>
2023-10-14 21:23:10 -0300

    feature: adjust envs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,8 +10,8 @@ export function cn(...inputs: ClassValue[]) {
 
 export function absoluteUrl(path: string) {
   if (typeof window !== 'undefined') return path
-  if (process.env.VERCEL_URL)
-    return `${process.env.VERCEL_URL}${path}`
+  if (process.env.APPLICATION_URL)
+    return `${process.env.APPLICATION_URL}${path}`
   return `http://localhost:${
     process.env.PORT ?? 3000
   }${path}`
