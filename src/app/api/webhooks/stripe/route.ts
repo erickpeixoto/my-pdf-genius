8cffec0ae97b35e409b1ea5adf4f26f9939b7233
Erick Eduardo <erickepeixoto@gmail.com>
2023-10-25 19:57:50 -0300

    feature: webhooks issue

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -1,14 +1,19 @@
 import { db } from '@/db'
 import { stripe } from '@/lib/stripe'
+import { headers } from 'next/headers'
 import type Stripe from 'stripe'
 
 export async function POST(request: Request) {
-  const body = await request.text();
-  const signature = request.headers.get('stripe-signature') ?? '';
 
+  const body = await request.text()
+  const signature = headers().get("Stripe-Signature") as string
 
-
+  console.log('Body:', body);
+  console.log('Signature:', signature);
+  
   let event: Stripe.Event
+
+
 
   try {
     event = stripe.webhooks.constructEvent(
