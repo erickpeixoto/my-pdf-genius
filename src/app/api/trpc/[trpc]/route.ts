c1933b6541156b4c7c6fa21258d1626cd35580e9
Erick Eduardo <erickepeixoto@gmail.com>
2023-10-28 12:47:31 -0300

    feature: fix signin

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,6 +1,5 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
 import { appRouter } from '@/trpc'
-import { Handlers } from '@highlight-run/node'
 
 const handler = (req: Request) =>
   fetchRequestHandler({
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -16,7 +16,7 @@ import { format } from "date-fns";
 import { Button } from "./ui/button";
 import { useState } from "react";
 import { Spinner } from "@nextui-org/react";
-import { H } from "@highlight-run/next/client";
+
 import { useUser } from "@clerk/nextjs";
 
 const Dashboard = () => {
@@ -39,12 +39,7 @@ const Dashboard = () => {
       setCurrentlyDeletingFile(null);
     },
   });
-console.log({user})
-  H.identify(user?.emailAddresses[0].emailAddress!, {
-    id: user?.id!,
-    avatar: user?.imageUrl!,
-  });
-  
+
   return (
     <main className="mx-auto max-w-7xl">
       {files && files?.length !== 0 ? (
